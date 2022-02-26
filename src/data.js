bab75f505b78b1a978b36d86503c7f33d5b87fa8
bvsswave <tannershahan@gmail.com>
2022-02-26 13:31:50 -0700

    Updated CSS files

---
+++ src/data.js
@@ -0,0 +1,115 @@
+import Voltron from './assets/images/voltron.png';
+import Infinity from './assets/images/infinity.png';
+import Todo from './assets/images/task-tracker.png';
+import WineBlog from './assets/images/wine-blogger.png';
+import WeatherScanner from './assets/images/weather-scanner.png';
+import FanTinder from './assets/images/fan-tinder.png';
+import Taskinator from './assets/images/taskinator.png';
+import PizzaHunt from './assets/images/pizza-hunt.png';
+import PwaBudgetTracker from './assets/images/pwa-budget-tracker.png';
+import TechBlog from './assets/images/tech-blog.png';
+import ShopShop from './assets/images/shop-shop.png';
+import GoogleBooks from './assets/images/book-search.png';
+import ZooKeepr from './assets/images/zoo-keepr.png';
+import WeatherDashboard from './assets/images/weather-dashboard.png';
+
+export default [
+    {
+        "id": 1,
+        "name": "FanTinder",
+        "image": FanTinder,
+        "github": "https://github.com/priyaravi23/FanTinder",
+        "deploy": "https://fan-tinder.herokuapp.com/"
+    },
+    {
+        "id": 2,
+        "name": "Weather Scanner",
+        "image": WeatherScanner,
+        "github": "https://github.com/priyaravi23/weather-app-client",
+        "deploy": "https://weather-detector-app.herokuapp.com/"
+    },
+    {
+        "id": 3,
+        "name": "Movie Rentals",
+        "image": Infinity,
+        "github": "https://github.com/priyaravi23/infinity-rental-client",
+        "deploy": "https://infinity-rental.herokuapp.com/"
+    },
+    {
+        "id": 4,
+        "name": "Wine Blog",
+        "image": WineBlog,
+        "github": "https://github.com/anitapeppercorn/wine-blogger",
+        "deploy": "https://secure-chamber-23014.herokuapp.com/"
+    },
+    {
+        "id": 5,
+        "name": "Voltron",
+        "image": Voltron,
+        "github": "https://github.com/priyaravi23/react-voltron-app",
+        "deploy": "https://react-voltron-app.herokuapp.com/"
+    },
+    {
+        "id": 6,
+        "name": "Shop Shop",
+        "image": ShopShop,
+        "github": "https://github.com/priyaravi23/shop-shop",
+        "deploy": "https://shop-shop-app.herokuapp.com/"
+    },
+    {
+        "id": 7,
+        "name": "Taskinator",
+        "image": Taskinator,
+        "github": "https://github.com/priyaravi23/taskinator",
+        "deploy": "https://priyaravi23.github.io/taskinator/"
+    },
+    {
+        "id": 8,
+        "name": "Pizza Hunt",
+        "image": PizzaHunt,
+        "github": "https://github.com/priyaravi23/pizza-hunt",
+        "deploy": "https://pizza-hunt-app.herokuapp.com/"
+    },
+    {
+        "id": 9,
+        "name": "PWA Budget Tracker",
+        "image": PwaBudgetTracker,
+        "github": "https://github.com/priyaravi23/pwa-budget-tracker",
+        "deploy": "https://budget-tracker-pwa-app.herokuapp.com/"
+    },
+    {
+        "id": 10,
+        "name": "Tech Blog",
+        "image": TechBlog,
+        "github": "https://github.com/priyaravi23/mvc-tech-blog",
+        "deploy": "https://mvc-tech-blog.herokuapp.com/"
+    },
+    {
+        "id": 11,
+        "name": "Task Tracker",
+        "image": Todo,
+        "github": "https://github.com/priyaravi23/react-todo-cypress",
+        "deploy": "https://react-todo-cypress.herokuapp.com/"
+    },
+    {
+        "id": 12,
+        "name": "Book Search Engine",
+        "image": GoogleBooks,
+        "github": "https://github.com/priyaravi23/react-book-search-engine",
+        "deploy": "https://react-book-search-engine.herokuapp.com/"
+    },
+    {
+        "id": 13,
+        "name": "Zoo Keepr",
+        "image": ZooKeepr,
+        "github": "https://github.com/priyaravi23/zookeepr",
+        "deploy": "https://zoo-keepr.herokuapp.com/"
+    },
+    {
+        "id": 14,
+        "name": "Weather Dashboard",
+        "image": WeatherDashboard,
+        "github": "https://github.com/priyaravi23/weather-dashboard",
+        "deploy": "https://priyaravi23.github.io/weather-dashboard/"
+    }
+]
